b8e0e637a7a7d8309eff42dfbbad4b56d9d5ba6f
Kira Shatulova <kira.shatulova@gmail.com>
2018-03-18 14:47:05 +0300

    finish hw

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+db.defaults({ users: [], skills: {}, products: [], emails: [] }).write();
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -24,16 +28,36 @@ router.get('/admin', function(req, res, next) {
 
 /* POST login page */
 router.post('/login', function (req, res) {
-  let currentUser = db.get('users').find({ email: req.body.email }).value()
+  let currentUser = db.get('users').find({ email: req.body.email }).value();
   if(currentUser && req.body.password === currentUser.password) {
     db.get('users').push(req.body).write();
-    console.log('Успешная авторизация')
-    res.redirect('/admin')
+    console.log('Успешная авторизация');
+    res.redirect('/admin');
   } else {
     db.get('users').push(req.body).write();
-    console.log('Создан новый пользователь')
-    res.redirect('/')
+    console.log('Создан новый пользователь');
+    res.redirect('/');
   }
+});
+
+/* POST admin/skills page */
+router.post('/admin/skills', function (req, res) {
+  db.set('skills', req.body).write();
+});
+
+/* POST admin page */
+router.post('/admin/upload', upload.single('photo'), function (req, res, next) {
+   let product = {
+    imgpath: req.file.path + '.' + req.file.originalname.split('.').pop(),
+    name: req.body.name,
+    price: req.body.price
+  }
+  db.get('products').push(product).write();
+});
+
+/* POST home page. */
+router.post('/', function (req, res) {
+  db.set('emails', req.body).write();
 });
 
 module.exports = router;
